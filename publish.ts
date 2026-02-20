62d38d7cf4c364447b10d91b79ac9ff39d6e84bb
stephen-spr <stephenraj.m@spritle.com>
2026-02-20 13:40:15 +0530

    chnages in script ttt

--- publish.ts
+++ publish.ts
@@ -1,7 +1,7 @@
 #!/usr/bin/env bun
 
 import { $ } from "bunssss"
-import { Script } from "@opencode-ai/script"
+import { Script } from "@opencode-ai/scripttttt"
 
 const highlightsTemplate = `
 <!--
